d3961afe3e42b377efd0ad089ca0b81b0f618984
bessax <andrebessax@gmail.com>
2023-12-27 13:34:34 -0300

    #Consumidor

--- Consumidor/Program.cs
+++ Consumidor/Program.cs
@@ -6,30 +6,20 @@ IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
         var configuration = hostContext.Configuration;
-        var fila = configuration.GetSection("MassTransit")["NomeFila"] ?? string.Empty;
-        var servidor = configuration.GetSection("MassTransit")["Servidor"] ?? string.Empty;
-        var usuario = configuration.GetSection("MassTransit")["Usuario"] ?? string.Empty;
-        var senha = configuration.GetSection("MassTransit")["Senha"] ?? string.Empty;
-        services.AddHostedService<Worker>();
-
+        var fila = configuration.GetSection("MassTransitAzure")["NomeFila"] ?? string.Empty;
+        var conexao = configuration.GetSection("MassTransitAzure")["Conexao"] ?? string.Empty;
+       
         services.AddMassTransit(x =>
         {
-            x.UsingRabbitMq((context, cfg) =>
-            {
-                cfg.Host(servidor, "/", h =>
-                {
-                    h.Username(usuario);
-                    h.Password(senha);
-                });
-                cfg.ReceiveEndpoint(fila, e =>
-                {
-                    e.Consumer<PedidoCriadoConsumidor>(context);
-                });
+           x.UsingAzureServiceBus((context, cfg) => {
+               
+               cfg.Host(conexao);
+               cfg.ReceiveEndpoint(fila, e =>
+               {
+                   e.Consumer<PedidoCriadoConsumidor>();             
+               });
 
-                cfg.ConfigureEndpoints(context);
-            });
-
-            x.AddConsumer<PedidoCriadoConsumidor>();
+           });
         });
     })
     .Build();
