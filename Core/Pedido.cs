d7dca815f38ad9ce166d06d938700f13f75a35d6
bessax <andrebessax@gmail.com>
2023-12-27 12:07:58 -0300

    Adicionar arquivos de projeto.

---
+++ Core/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core;
+
+public class Pedido
+{
+    public int Id { get; set; }
+    public Usuario? Usuario { get; set; }
+    public DateTime? DataCriacao { get; set; }
+
+    public Pedido(int id, Usuario? usuario, DateTime? dataCriacao)
+    {
+        Id = id;
+        Usuario = usuario;
+        DataCriacao = dataCriacao;
+    }
+    override public string ToString()
+    {
+        return $"Id: {Id}, Usuario: {Usuario}, DataCriacao: {DataCriacao}";
+    }
+}
